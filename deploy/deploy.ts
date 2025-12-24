57fb0aac71ce4a264171c5f00ae3bdbce6e33930
fabiomorettii <kbooxqtr4150@outlook.com>
2025-12-24 11:00:07 +0800

    Wire up ConfidentialFundraiser deploy script

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedFundraiser = await deploy("ConfidentialFundraiser", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`ConfidentialFundraiser contract: `, deployedFundraiser.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_confidentialFundraiser"; // id required to prevent reexecution
+func.tags = ["ConfidentialFundraiser"];
